1ee4ca43ba8bda040b2d8571872021cadaeba704
Abhishek Yadav <abhishekcms611@gmail.com>
2020-09-01 15:20:20 +0530

    updated tests

--- inventory/src/test/java/com/store/inventory/InventoryApplicationTests.java
+++ inventory/src/test/java/com/store/inventory/InventoryApplicationTests.java
@@ -45,5 +45,79 @@ class InventoryApplicationTests {
 		assert (actual.toString().equals(expected.toString()));
 	}
 
+	@Test
+	void createOrderTestValid(){
+		Book book = new Book("isbn",10);
+		CatalogueOrder order = new CatalogueOrder("user","isbn",4);
+		when(bookRepository.existsById("isbn")).thenReturn(true);
+		when(bookRepository.findById("isbn")).thenReturn(java.util.Optional.of(new Book("isbn", 10)));
+		when(bookRepository.save(book)).thenReturn(book);
+
+		bookService.createOrder(order);
+
+		when(bookRepository.existsById("isbn")).thenReturn(true);
+		when(bookRepository.findById("isbn")).thenReturn(java.util.Optional.of(new Book("isbn", 10-4)));
+		when(bookRepository.save(book)).thenReturn(book);
+
+		Book actual = bookService.find("isbn");
+		Book expected = new Book("isbn",6);
+
+		assert(actual.toString().equals(expected.toString()));
+	}
+
+	@Test
+	void createOrderTestInvalid(){
+		Book book = new Book("isbn",10);
+		CatalogueOrder order = new CatalogueOrder("user","isbn",40);
+		when(bookRepository.existsById("isbn")).thenReturn(true);
+		when(bookRepository.findById("isbn")).thenReturn(java.util.Optional.of(new Book("isbn", 10)));
+		when(bookRepository.save(book)).thenReturn(book);
+
+		bookService.createOrder(order);
+
+		when(bookRepository.existsById("isbn")).thenReturn(true);
+		when(bookRepository.findById("isbn")).thenReturn(java.util.Optional.of(new Book("isbn", 10)));
+
+		Book actual = bookService.find("isbn");
+		Book expected = new Book("isbn",10);
+
+		assert(actual.toString().equals(expected.toString()));
+
+		CatalogueOrder orderInvalid  = new CatalogueOrder("user","isbn2",10);
+		when(bookRepository.existsById("isbn2")).thenReturn(false);
+		bookService.createOrder(orderInvalid);
+
+		assertThrows(BookNotFoundException.class,()->bookService.find("isbn2"));
+	}
+
+	@Test
+	void rollBackInventoryTest(){
+		Book book = new Book("isbn",2);
+		CatalogueOrder order = new CatalogueOrder("user","isbn",2);
+		when(bookRepository.existsById("isbn")).thenReturn(true);
+		when(bookRepository.findById("isbn")).thenReturn(java.util.Optional.of(new Book("isbn", 10)));
+		when(bookRepository.save(book)).thenReturn(book);
+
+		bookService.rollBack(order);
+		when(bookRepository.findById("isbn")).thenReturn(java.util.Optional.of(new Book("isbn", 12)));
+
+		Book actual = bookService.find("isbn");
+		Book expected = new Book("isbn",12);
+
+		assert(actual.toString().equals(expected.toString()));
+
+		CatalogueOrder order2 = new CatalogueOrder("user","isbn2",10);
+		when(bookRepository.existsById("isbn2")).thenReturn(false);
+		bookService.rollBack(order2);
+		when(bookRepository.existsById("isbn2")).thenReturn(true);
+		when(bookRepository.findById("isbn2")).thenReturn(java.util.Optional.of(new Book("isbn2", 10)));
+
+		expected = new Book("isbn2",10);
+		actual = bookService.find("isbn2");
+
+		assert (expected.toString().equals(actual.toString()));
+		
+	}
+
 
 }
